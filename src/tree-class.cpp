929dc13ad5bd1761124a2fcb23211616447320b0
Brian Mannakee <mannakee@email.arizona.edu>
2018-06-29 15:50:26 -0700

    ugh. so broken

--- src/tree-class.cpp
+++ src/tree-class.cpp
@@ -1,8 +1,10 @@
 // [[Rcpp::depends(BH)]]
 #include <Rcpp.h>
-#include <boost/uuid/uuid.hpp>
-#include <boost/uuid/uuid_generators.hpp>
+#include <boost>
+#include <random>
+#include <array>
 using namespace Rcpp;
+using boost::uuids;
 
 // This is a simple example of exporting a C++ function to R. You can
 // source this function into an R session using the Rcpp::sourceCpp
@@ -14,27 +16,33 @@ using namespace Rcpp;
 //   http://gallery.rcpp.org/
 //
 typedef std::vector<double> double_vec;
-typedef std::vecotr<Mutation> mut_vec;
+// TODO: This needs to be passed in once implemented in Cell
+static const std::array<double,4> spectrum = {.25,.25,.25,.25};
 
 
-Class Mutation{
-public:
-  int muttype;
-  boost::uuids::uuid mid;
-  Mutation(): mid(boost::uuids::random_generator()()){
+class Mutation{
+  private:
+    std::default_random_engine generator;
+    std::discrete_distribution<int> distribution(spectrum.begin(),spectrum.end());
+
+  public:
+    int muttype;
+    boost::uuids::uuid mid;
+    Mutation(): mid(boost::uuids::random_generator()()){
     // TODO: Pass in spectrum in a way that makes the distribution work
-    muttype = generator(distribution)
-  }
+      muttype = generator(dist);
+    };
 
-private:
-  std::default_random_engine generator;
-  std::discrete_distribution distribution {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-}
 
-Class Cell {
-public:
-  Cell(int size_, int mutrate_, double reprate_, mut_vec mutations_):
-  size(size_), mutrate(mutrate_), reprate(reprate_), mutations(mutations_){
+};
 
-  }
+
+typedef std::vector<Mutation> mut_vec;
+
+class Cell {
+  public:
+    Cell(int size_, int mutrate_, double reprate_, mut_vec mutations_):
+    size(size_), mutrate(mutrate_), reprate(reprate_), mutations(mutations_){};
+
+  private:
 }
