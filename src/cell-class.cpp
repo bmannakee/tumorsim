073ad329672be5a21858c321bd4553503b7a9eb3
Brian Mannakee <brianmannakee@Brians-iMac.local>
2018-07-01 14:44:54 -0700

    add LinkingTo:BH and start Cell class

---
+++ src/cell-class.cpp
@@ -0,0 +1,52 @@
+// [[Rcpp::depends(BH)]]
+#include <Rcpp.h>
+#include <boost>
+#include <random>
+#include <array>
+using namespace Rcpp;
+using boost::uuids;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+typedef std::vector<double> double_vec;
+// TODO: This needs to be passed in once implemented in Cell
+static const std::array<double,4> spectrum = {.25,.25,.25,.25};
+
+
+class Mutation{
+private:
+  std::default_random_engine generator;
+  std::discrete_distribution<int> distribution(spectrum.begin(),spectrum.end());
+
+public:
+  int muttype;
+  boost::uuids::uuid mid;
+  Mutation(): mid(boost::uuids::random_generator()()){
+    // TODO: Pass in spectrum in a way that makes the distribution work
+    muttype = generator(dist);
+  };
+
+
+};
+
+
+typedef std::vector<Mutation> mut_vec;
+
+class Cell {
+    int mutrate;
+    double reprate;
+    mut_vec mutations;
+  public:
+    Cell(int mutrate_, double reprate_, mut_vec mutations_):
+    mutrate(mutrate_), reprate(reprate_), mutations(mutations_){}
+
+    int get_mutrate() {return mutrate;}
+    double get_reprate() {return reprate;}
+}
